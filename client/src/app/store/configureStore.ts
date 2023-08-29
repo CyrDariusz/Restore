aba1f3385432268523e2e5aa9e7fb9a8e5ff6857
CyrDariusz <cyrnek.dariusz@wp.pl>
2023-08-29 14:09:22 +0200

    7th commit

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -11,8 +11,8 @@ import { catalogSlice } from "../../features/catalog/catalogSlice";
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    basket:basketSlice.reducer,
-    catalog:catalogSlice.reducer
+    basket: basketSlice.reducer,
+    catalog: catalogSlice.reducer,
   },
 });
 
@@ -20,4 +20,4 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
